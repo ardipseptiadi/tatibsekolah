dd2d69abf261bdcdfb5f06b332582fb5177e6e3e
ardiseptiadi <ardipseptiadi@gmail.com>
2018-11-09 03:44:17 +0700

    add controller kelas

--- application/controllers/Kelas.php
+++ application/controllers/Kelas.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class KategoriPelanggaran extends CI_Controller {
+class Kelas extends CI_Controller {
 	private $head;
 	function __construct(){
 		parent::__construct();
